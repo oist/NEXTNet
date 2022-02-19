d50a8fc270f9fded258215d667cd7edb81b68a2d
samuelcure <samuel.cure@me.com>
2022-02-19 17:14:23 +0900

    Initial Commit

---
+++ epidemics_update/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  epidemics_update
+//
+//  Created by Cure Samuel Cyrus on 2022/02/19.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
