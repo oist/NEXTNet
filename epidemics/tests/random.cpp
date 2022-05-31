86e72397644acdac29ec31e13ca79ada4b82b5a9
Florian G. Pflug <fgp@phlo.org>
2022-05-31 17:30:10 +0900

    Added test for the case pinfinity > 0

--- epidemics/tests/random.cpp
+++ epidemics/tests/random.cpp
@@ -35,7 +35,7 @@ TEST_CASE("Lognormal distribution nonconditional", "[random]") {
     // TODO: Check that t=0, m=1 produces the correct mean and variance
 }
 
-TEST_CASE("Lognormal distribution incremental sampling", "[random]") {
+TEST_CASE("Lognormal distribution incremental sampling (pinf=0)", "[random]") {
     std::mt19937 engine;
 
     transmission_time_lognormal ln(5, 1);
@@ -88,6 +88,84 @@ TEST_CASE("Lognormal distribution incremental sampling", "[random]") {
         REQUIRE(samples_seq[j].size() == N);
         const auto iid_mv = mean_variance(samples_iid[j].begin(), samples_iid[j].end());
         const auto seq_mv = mean_variance(samples_seq[j].begin(), samples_seq[j].end());
+        const double z = ((iid_mv.first - seq_mv.first) * sqrt(N) /
+                          (iid_mv.second + seq_mv.second));
+        REQUIRE(std::abs(z) <= 3);
+    }
+}
+
+TEST_CASE("Lognormal distribution incremental sampling (pinf>0)", "[random]") {
+    std::mt19937 engine;
+
+    const double PINF=0.2;
+
+    transmission_time_lognormal ln(5, 1, PINF);
+
+    const int N = 10000;
+    const int K = 3;
+
+    /* Sample ordered pairs (s_1, ..., s_k) from the transmission time
+     * distribution by taking two independent samples and ordering them.
+     */
+    std::vector<std::vector<double>> samples_iid(K, std::vector<double> {});
+    for(int i=0; i < N; ++i) {
+        std::vector<double> s;
+        for(int j=0; j < K; ++j) {
+            const double t = ln.sample(engine, 0, 1);
+            REQUIRE(t);
+            s.push_back(t);
+        }
+        REQUIRE(s.size() == K);
+        std::sort(s.begin(), s.end());
+        for(int j=0; j < K; ++j)
+            samples_iid[j].push_back(s[j]);
+    }
+
+    /* Sample ordered pairs (s_1, ..., s_k) from the transmission time
+     * distribution by sampling s_i from the conditional distribution
+     * with t = s_(i-1) and m = k-i+1. In other words, sample first the
+     * minimum, then the second-largest sample conditioned on the value
+     * of the minimum and so on. Note that due to the way we defined
+     * transmission_time(), the conditional version of the distribtion
+     * returns the *difference* between the condition t and the samples
+     * value.
+     */
+    std::vector<std::vector<double>> samples_seq(K, std::vector<double> {});
+    for(int i=0; i < N; ++i) {        
+        double t = 0;
+        for(int j=0; j < K; ++j) {
+            const double d = std::isfinite(t) ? ln.sample(engine, t, K-j) : INFINITY;
+            REQUIRE(d >= 0);
+            t = t + d;
+            samples_seq[j].push_back(t);
+        }
+    }
+
+    /* The two sampling methods should give the same results.
+     * Compare mean and variance between the two sampling methods to verify
+     */
+    for(int j=0; j < K; ++j) {
+        REQUIRE(samples_iid[j].size() == N);
+        REQUIRE(samples_seq[j].size() == N);
+        
+        /* Check if the number of finite samples agrees with a z-test assuming binomial variance */
+        const double iid_fin = std::count_if(samples_iid[j].begin(), samples_iid[j].end(),
+                                             [] (auto &e) { return std::isfinite(e); });
+        const double seq_fin = std::count_if(samples_seq[j].begin(), samples_seq[j].end(),
+                                             [] (auto &e) { return std::isfinite(e); });
+        const double z_fin = ((iid_fin - seq_fin) /
+                              (sqrt(iid_fin*(N - iid_fin)/N) + sqrt(seq_fin*(N - seq_fin)/N)));
+        REQUIRE(std::abs(z_fin) <= 3);
+        
+        /* Check if the means of the finite samples agree with a z-test */
+        auto copy_iid = samples_iid[j];
+        copy_iid.erase(std::remove_if(copy_iid.begin(), copy_iid.end(), [] (double t) { return std::isinf(t); }),
+                       copy_iid.end());
+        const auto iid_mv = mean_variance(copy_iid.begin(), copy_iid.end());
+        auto copy_seq = samples_seq[j];
+        copy_seq.erase(std::remove_if(copy_seq.begin(), copy_seq.end(), [] (double t) { return std::isinf(t); }),
+                       copy_seq.end());
+        const auto seq_mv = mean_variance(copy_seq.begin(), copy_seq.end());
         const double z = ((iid_mv.first - seq_mv.first) * sqrt(N) /
                           (iid_mv.second + seq_mv.second));
         REQUIRE(std::abs(z) <= 3);
