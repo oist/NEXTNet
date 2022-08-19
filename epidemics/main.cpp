f13b4836e876bf5da8486232d95ec2230d86a877
Samuel Cure <samuelcyrus.cure@oist.jp>
2022-08-19 10:52:31 +0900

    added an example of the nmga in the main method

--- epidemics/main.cpp
+++ epidemics/main.cpp
@@ -10,6 +10,7 @@
 #include "analysis.h"
 #include "simulation.h"
 #include "graph.h"
+#include "nMGA.h"
 
 using namespace std;
 
@@ -19,18 +20,42 @@ using namespace std;
 
 int main(int argc, const char * argv[]) {
 
-    int n = 1000;
-    if(argc == 2){
-        n = atoi(argv[1]);
-    }
-    
-
-    
+    int size = 1000;
     int degree = 3;
     double mean = 10;
     double variance = 1.0;
+    int threshold = 100;
+
+    // User's input
+    if(argc == 2){
+        size = atoi(argv[1]);
+    } else if (argc == 3){
+            size = atoi(argv[1]);
+            threshold = atoi(argv[2]);
+    }
     
-   
+
+
+    erdos_reyni network(size,degree, engine);
+    transmission_time_lognormal psi(mean, variance); 
+    simulate_nmga simulation(network, psi,threshold);
+
+
+    simulation.add_infections({ std::make_pair(0, 0.0)});
+    
+    std::vector<double> time_trajectory({});
+    std::vector<double> vertex_path({});
+    for (int i =0 ; i< size; i++) {
+        auto point = simulation.step(engine);
+        if (point.second != INFINITY) {
+            continue;
+        }
+        break;
+    }
+
+
+    cout << " done \n";
+
     return 0;
 }
 
