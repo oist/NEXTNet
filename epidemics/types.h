c5a73b7cf0d69b4537098707f9eab0744f97c9dc
Florian G. Pflug <florian.pflug@oist.jp>
2022-10-24 16:45:26 +0900

    Allow custom RNG types to be used

--- epidemics/types.h
+++ epidemics/types.h
@@ -9,7 +9,6 @@
 
 #include "stdafx.h"
 
-typedef std::mt19937 rng_t;
 typedef int node_t;
 typedef int index_t;
 typedef double interval_t;
@@ -25,3 +24,58 @@ struct event_t {
     node_t node;
     absolutetime_t time;
 };
+
+/******************************
+ * rng_t - the RNG (random number generator) to use
+ *
+ * The RNG to use can either be selected from a list of supported RNGs by
+ * defining RNG to be one of the RNG_* constants below. RNG_STDMT19937 for
+ * example is the 32-bit Mersenne Twister from the C++11 random library.
+ * 
+ * Or a custom RNG can be provided by defining RNG=RNG_CUSTOM, and implementing
+ * the function std::uint32_t rng_t::operator()(). 
+ * Define RNG=RNG_STDMT19937 to use the Mersenne Twister from the C++ stdlib,
+ * or define RNG=RNG_CUSTOM and RNG_TYPE=<the RNG engine type>.
+ ******************************/
+
+#define RNG_STDMT19937 1
+#define RNG_CUSTOM 2
+
+#if !defined(RNG) || (RNG == RNG_STDMT19937)
+
+/* Use C++ mersenne twister (default) */
+#define RNG RNG_STDMT19937
+typedef std::mt19937 rng_t;
+
+#elif RNG == RNG_CUSTOM
+
+/* Forward-declare state type */
+struct RNG_STATE_TYPE;
+
+/* Custom RNG type. operator() must be implemented by the user */
+struct rng_t {
+  typedef std::uint32_t result_type;
+
+  static constexpr result_type min() { return 0; }
+  
+  static constexpr result_type max() { return UINT32_MAX; }
+
+  result_type operator()();
+  
+#if defined(RNG_STATE_TYPE)
+  typedef RNG_STATE_TYPE state_type;
+  
+  typedef std::uniqie_ptr<state_type> state_ptr; 
+  
+  rng_t(state_ptr state_) :state(std::move(state_)) {}
+  
+  state_ptr state;
+#endif
+};
+  
+#else
+
+/* Unknown RNG */
+#error "unknown RNG " ## RNG
+
+#endif
