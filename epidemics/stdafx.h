18837b1f7b93a6e74402cf78293abc69fdc5e0de
Florian G. Pflug <florian.pflug@oist.jp>
2022-10-24 12:58:41 +0900

    Deactive warnings on GCC as well when including boost

--- epidemics/stdafx.h
+++ epidemics/stdafx.h
@@ -22,12 +22,26 @@
 #include <numeric>
 #include <chrono>
 
+#if defined(__clang__)
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Weverything"
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+#elif defined(__GNUC__) || defined(__GNUG__)
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wall"
+#pragma GCC diagnostic ignored "-Wsign-compare"
+#pragma GCC diagnostic ignored "-Wparentheses"
+#endif
+
 #include <boost/math/distributions.hpp>
 #include <boost/math/statistics/univariate_statistics.hpp>
 #include <boost/math/tools/roots.hpp>
+
+#if defined(__clang__)
 #pragma clang diagnostic pop
+#elif defined(__GNUC__) || defined(__GNUG__)
+#pragma GCC diagnostic pop
+#endif
 
 /* Make boost::math available as simply bm */
 namespace bm = boost::math;
