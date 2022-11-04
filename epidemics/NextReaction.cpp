a07dc5eae374b2d773bc5cc4b0bc31fa06727ef6
Samuel Cure <samuelcyrus.cure@oist.jp>
2022-11-04 16:57:19 +0900

    " added an efficient method to generate epidemics in a Mean-Field regime

--- epidemics/NextReaction.cpp
+++ epidemics/NextReaction.cpp
@@ -187,3 +187,122 @@ const transmission_time& simulate_next_reaction::transmission_time() const {
 const transmission_time* simulate_next_reaction::reset_time() const {
     return rho;
 }
+
+
+
+// --------------- MEAN FIELD ---------------------
+
+
+
+std::optional<event_t> simulate_next_reaction_mean_field::step(rng_t& engine) {
+    while (true) {
+        /* If there are no more infection times, stop */
+        if (active_edges.empty())
+			return std::nullopt;
+
+        /* Fetch the next infection/reset time, i.e. the time where the next edge fires */
+        const auto next = active_edges.top();
+        active_edges.pop();
+        
+        /* Perform event */
+        std::optional<event_t> result;
+        switch (next.kind) {
+            case event_kind::infection:
+            case event_kind::outside_infection:
+				result = step_infection(next, engine);
+				break;
+			case event_kind::reset:
+				result = step_reset(next, engine);
+				break;
+            default: throw std::logic_error("invalid event kind");
+        }
+
+        /* Return event (unless skipped, in which case we continue) */
+        if (result)
+            return result;        
+    }
+}
+
+std::optional<event_t> simulate_next_reaction_mean_field::step_infection(const active_edges_entry& next, rng_t& engine) {
+    /*
+     * Here, the variables have the following meaning:
+     *   next.time: The current time, i.e. the time at which the edge fired
+     *   next.node: The node that just got infected (putatively, since it might already be)
+     */
+    
+    /* Check if the putatively infected node is already infected, if so we're done */
+    if (is_infected(next.node))
+        return std::nullopt;
+
+    /* Node becomes infected.
+     * Mark the node as infected, generate reset time, insert recovery
+     */
+    infected.insert(next.node);
+    const interval_t tau_reset = (rho ? rho->sample(engine, 0, 1) : INFINITY);
+    if (tau_reset < INFINITY) {
+        active_edges_entry e;
+        e.kind = event_kind::reset;
+        e.time = next.time + tau_reset;
+        e.node = next.node;
+        active_edges.push(e);
+    }
+
+    /* Instead of generating k=Poi(R0) and then sampling without replacement k nodes,
+    we can sample them using the geomertic distribution.*/
+    std::geometric_distribution<> skip_edge(p); // comment: equals 0 with prob. p
+
+    for (node_t node=0; node < N; node++) {
+        int s = skip_edge(engine);
+        if (s + node >= N)
+            break;
+        node += (node == next.node ? 1 + s : s);
+        const interval_t tau_inf = psi.sample(engine, 0, 1); // sample simple r.v from psi distribution with age 0
+        if (tau_inf > tau_reset) // transmission will not happen. (Note: in our simulations we often choose rho s.t Prob[tau_inf > tau_reset] ~ 1).
+            continue;
+        active_edges_entry e;
+        e.kind = event_kind::infection;
+        e.time = next.time + tau_inf;
+        e.node = node;
+        active_edges.push(e);
+    }
+
+    /* Return the infection event */
+    return event_t { .kind = next.kind, .node = next.node, .time = next.time };
+}
+
+std::optional<event_t> simulate_next_reaction_mean_field::step_reset(const active_edges_entry& next, rng_t& engine) {
+    /* The node cannot yet have resetted */
+    assert(is_infected(next.node));
+    
+    /* Mark node as no longer infected */
+    infected.erase(next.node);
+
+    /* Return the reset event */
+    return event_t { .kind = event_kind::reset, .node = next.node, .time = next.time };
+}
+
+void simulate_next_reaction_mean_field::add_infections(const std::vector<std::pair<node_t, absolutetime_t>>& v) {
+    for(const auto& ve: v) {
+        active_edges_entry e;
+        e.kind = event_kind::outside_infection;
+        e.time = ve.second;
+        e.node = ve.first;
+        active_edges.push(e);
+    }
+}
+
+bool simulate_next_reaction_mean_field::is_infected(node_t node) const {
+    return (infected.find(node) != infected.end());
+}
+
+// graph& simulate_next_reaction::get_network() const {
+//     return network;
+// }
+
+const transmission_time& simulate_next_reaction_mean_field::transmission_time() const {
+    return psi;
+}
+
+const transmission_time* simulate_next_reaction_mean_field::reset_time() const {
+    return rho;
+}
--- epidemics/NextReaction.h
+++ epidemics/NextReaction.h
@@ -80,3 +80,66 @@ private:
 	
 	std::optional<event_t> step_reset(const active_edges_entry& next, rng_t& engine);
 };
+
+
+class simulate_next_reaction_mean_field : public simulation_algorithm {
+public:
+    simulate_next_reaction_mean_field(int N_, double R0_, const class transmission_time& psi_,
+                           const class transmission_time* rho_ = nullptr)
+        :N(N_), R0(R0_), psi(psi_), rho(rho_)
+    {}
+    
+    virtual const class transmission_time& transmission_time() const;
+
+    virtual const class transmission_time* reset_time() const;
+
+    virtual void add_infections(const std::vector<std::pair<node_t, absolutetime_t>>& v);
+    
+    virtual std::optional<event_t> step(rng_t& engine);
+
+    virtual bool is_infected(node_t) const;
+
+    const int N;
+    
+    const double R0;
+    
+private:
+    const double p = R0 / (N - 1);
+    const class transmission_time& psi;
+    const class transmission_time* rho = nullptr;
+    std::unordered_set<node_t> infected;
+    
+    struct active_edges_entry {
+        /*
+         * Event kind represented by this edge (infection or reset)
+         * Reset events are self-loops, a consequently they obey
+         * source_node=-1, neighbour_index=-1, neighbours_remaining=0.
+         */
+        event_kind kind;
+
+        /*
+         * Absolute time of infection
+         */
+        absolutetime_t time;
+        
+        /*
+         * Node that is (putatively) infected
+         */
+        node_t node;
+                
+        bool operator< (const active_edges_entry& o) const { return time < o.time; }
+        bool operator<= (const active_edges_entry& o) const { return time <= o.time; }
+        bool operator== (const active_edges_entry& o) const { return time == o.time; }
+        bool operator!= (const active_edges_entry& o) const { return time != o.time; }
+        bool operator>= (const active_edges_entry& o) const { return time >= o.time; }
+        bool operator> (const active_edges_entry& o) const { return time > o.time; }
+    };
+    
+    std::priority_queue<active_edges_entry, std::deque<active_edges_entry>,
+                        std::greater<active_edges_entry>>
+      active_edges;
+	
+	std::optional<event_t> step_infection(const active_edges_entry& next, rng_t& engine);
+	
+	std::optional<event_t> step_reset(const active_edges_entry& next, rng_t& engine);
+};
