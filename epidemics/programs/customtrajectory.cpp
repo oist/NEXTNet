7504d8f9ac80e4a948e8422a7045971599af5a86
Samuel Cure <samuelcyrus.cure@oist.jp>
2022-11-30 19:42:19 +0900

    added file for custom trajectory

---
+++ epidemics/programs/customtrajectory.cpp
@@ -0,0 +1,80 @@
+#include "stdafx.h"
+#include "random.h"
+#include "analysis.h"
+#include "nMGA.h"
+#include "NextReaction.h"
+
+using namespace std;
+
+int program_customtrajectory(int argc, const char * argv[]) {
+    rng_t engine;
+
+    /* input filename of the adjacency list csv (the nth row represents the link to the nth node, e.g. 4,5,10,345) */ 
+    string input_filename = argv[2];
+
+    // 0 -> SI, 1 -> SIR, 2 -> SIS
+    int model = atoi(argv[3]);
+
+    /* Parameters for the epidemic*/ 
+	double MEAN_INFECTION = atof(argv[4]);
+    double VARIANCE_INFECTION = atof(argv[5]);
+    double MEAN_RECOVERY = atof(argv[6]);
+    double VARIANCE_RECOVERY = atof(argv[7]);
+
+    // Initial percentage of infected (number from 1 to 100);
+    double I0 = atof(argv[8]);
+
+    // Maximum time in the system (only used for the SIS, else INFINITY.)
+    double TMAX = atof(argv[9]);
+    string output_filename = argv[10];
+
+    transmission_time_gamma psi(MEAN_INFECTION, VARIANCE_INFECTION);
+    transmission_time_gamma rho(MEAN_RECOVERY, VARIANCE_RECOVERY);
+
+    switch (model)
+    {
+    case 0:
+        simulate_trajectory(engine, [input_filename,psi](rng_t& engine) {
+            struct {
+                std::unique_ptr<graph> nw;
+                std::unique_ptr<simulation_algorithm> simulator;
+            } env;
+            env.nw.reset(new imported_network(input_filename));
+            env.simulator.reset(new simulate_next_reaction(*env.nw,psi));
+            return env;
+            }, I0, INFINITY,output_filename);
+        break;
+    case 1:
+        simulate_trajectory(engine, [input_filename,psi,rho](rng_t& engine) {
+			struct {
+				std::unique_ptr<graph> nw;
+				std::unique_ptr<simulation_algorithm> simulator;
+			} env;
+			env.nw.reset(new imported_network(input_filename));
+            const bool shuffle_neighbours = false;
+            const bool concurrent_edges = false;
+            const bool SIR = true;
+            env.simulator.reset(new simulate_next_reaction(*env.nw,psi,&rho,shuffle_neighbours,concurrent_edges,SIR));
+			return env;
+		}, I0, INFINITY, output_filename);
+        break;
+    case 2:
+        simulate_trajectory(engine, [input_filename,psi,rho](rng_t& engine) {
+			struct {
+				std::unique_ptr<graph> nw;
+				std::unique_ptr<simulation_algorithm> simulator;
+			} env;
+			env.nw.reset(new imported_network(input_filename));
+            const bool shuffle_neighbours = true;
+            const bool concurrent_edges = false;
+            const bool SIR = false;
+            env.simulator.reset(new simulate_next_reaction(*env.nw,psi,&rho,shuffle_neighbours,concurrent_edges,SIR));
+			return env;
+		}, I0, TMAX, output_filename);
+        break;
+    default:
+        break;
+    }
+
+    return 0;
+}
