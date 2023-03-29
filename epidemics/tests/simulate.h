d343671f6746cd4d7ecbc2f5298b6e03301b9e9e
Florian G. Pflug <fgp@phlo.org>
2023-03-29 16:58:24 +0900

    Added version of average_trajectories() for dynamic networks

--- epidemics/tests/simulate.h
+++ epidemics/tests/simulate.h
@@ -138,3 +138,85 @@ average_trajectories(rng_t& engine, Factory factory,
 	}
 }
 
+/**
+ * @brief simulate and return transmission times
+ * @param engine RNG engine
+ * @param psi transmission time distribution
+ * @param T stopping time
+ * @param args parameters to pass to the network
+ * @return a pair of pairs containing (1) ordered transmission times
+ *         and (2) total number of infections
+ */
+template<typename Factory, typename DeltaInfectedFunctor>
+void
+average_trajectories(rng_t& engine, Factory factory, DeltaInfectedFunctor f,
+					 std::vector<absolutetime_t> &times,
+					 std::vector<double>& cumulative_infected,
+					 std::vector<double>& current_infected,
+					 absolutetime_t Tmax, int runs = 1, int output_every = -1)
+{
+	typedef std::pair<double, double> pair_t;
+	typedef std::vector<pair_t> pairs_t;
+	
+	if (output_every < 0)
+		output_every = runs;
+	
+	// Run <runs> simulations, collect a large vector of
+	// (time, delta) pairs which allow easy averaging
+	auto results_par = parallel<pairs_t>(runs, engine, [factory, f, Tmax](rng_t& thread_engine) {
+		pairs_t r = {};
+
+		// Create simulation environment
+		auto s = factory(thread_engine);
+		auto& sim = *s.simulator;
+		
+		// Run simulation, collect pairs
+		while (true) {
+			// Perform step, stop if no more events
+			const auto point = sim.step(thread_engine);
+			if (!point)
+				break;
+			// Convert event to (time, delta) pair, stop if time exceeds Tmax
+			const pair_t p = f(*point);
+			if (p.first > Tmax)
+				break;
+			// Only store points with non-zero delta
+			if (p.second != 0.0)
+				r.push_back(p);
+		}
+
+		return r;
+	});
+	
+	// Merge results
+	pairs_t results;
+	for(const auto& r: results_par)
+	  std::copy(r.begin(), r.end(), std::back_inserter(results));
+
+	// Sort by times
+	std::sort(results.begin(), results.end(), [](const pair_t& a, pair_t& b) {
+		return a.first < b.first;
+	});
+	
+	// Output results
+	int i = 0;
+	double cur_inf = 0.0;
+	double cum_inf = 0.0;
+	for(const pair_t& p: results) {
+		/* Track number of infected */
+		const double t = p.first;
+		const double d = p.second / (double)runs;
+		cur_inf += d;
+		cum_inf += std::max(d, 0.0);
+
+		/* Only output every <output_every>-th element */
+		if (++i != output_every)
+			continue;
+		i = 0;
+
+		times.push_back(t);
+		cumulative_infected.push_back(cum_inf);
+		current_infected.push_back(cur_inf);
+	}
+}
+
