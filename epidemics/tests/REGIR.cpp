374f90e02103fd2a66ea9753cb592b20c9a25cf1
Florian G. Pflug <fgp@phlo.org>
2023-06-22 18:21:29 +0900

    Changed "Plot SIS single trajectory (REGIR)" to ""Plot SIS average trajectory (REGIR)"
    Also modified the parameters

--- epidemics/tests/REGIR.cpp
+++ epidemics/tests/REGIR.cpp
@@ -5,6 +5,7 @@
 
 #include "random.h"
 #include "REGIR.h"
+#include "NextReaction.h"
 
 
 TEST_CASE("Stability (REGIR)", "[REGIR]") {
@@ -67,22 +68,26 @@ TEST_CASE("Plot large-population SIR mean-field (REGIR)", "[REGIR]") {
 #endif
 
 #if ENABLE_PLOTTING
-TEST_CASE("Plot SIS single trajectory (REGIR)", "[REGIR]") {
+TEST_CASE("Plot SIS average trajectory (REGIR)", "[REGIR]") {
 	using namespace std::string_literals;
 	std::mt19937 engine;
-
+	
+	const std::size_t M = 20;
 	const int N = 100;
-	const double T = 25;
+	const double T = 15;
 	const double R0 = 3;
 
-	const double MEAN = 3;
-	const double VARIANCE = 1;
-	const double MEAN_rho = 10;
-	const double VARIANCE_rho = 1;
+	const double MEAN = 2;
+	const double VARIANCE = 2;
+	const double MEAN_rho = 5;
+	const double VARIANCE_rho = 3;
 	transmission_time_gamma psi(MEAN, VARIANCE);
 	transmission_time_gamma rho(MEAN_rho, VARIANCE_rho);
-
-	/* Simulate using REGIT once */
+	
+	simulate_regir::params p_regir;
+	p_regir.approximation_threshold = 100;
+	
+	/* Simulate using REGIR once */
 	std::vector<double> t_sim, y_sim_new, y_sim_total;
 	average_trajectories(engine, [&](rng_t& engine){
 		struct {
@@ -90,12 +95,25 @@ TEST_CASE("Plot SIS single trajectory (REGIR)", "[REGIR]") {
 			std::unique_ptr<simulation_algorithm> simulator;
 		} env;
 		env.nw.reset(new erdos_reyni(N, R0, engine));
-		env.simulator.reset(new simulate_regir(*env.nw, psi, &rho));
+		env.simulator.reset(new simulate_regir(*env.nw, psi, &rho, p_regir));
 		return env;
-	}, t_sim, y_sim_new, y_sim_total, T);
-
-	plot("regir.sis.single.pdf", "SIS single trajectory [REGIR]", [&](auto& gp, auto& p) {
+	}, t_sim, y_sim_new, y_sim_total, T, M);
+	
+	/* Simulate using NextReaction once */
+	std::vector<double> t_sim_nr, y_sim_new_nr, y_sim_total_nr;
+	average_trajectories(engine, [&](rng_t& engine){
+		struct {
+			std::unique_ptr<graph> nw;
+			std::unique_ptr<simulation_algorithm> simulator;
+		} env;
+		env.nw.reset(new erdos_reyni(N, R0, engine));
+		env.simulator.reset(new simulate_next_reaction(*env.nw, psi, &rho));
+		return env;
+	}, t_sim_nr, y_sim_new_nr, y_sim_total_nr, T, M);
+	
+	plot("regir.sis.average.pdf", "SIS single trajectory [REGIR]", [&](auto& gp, auto& p) {
 		p.add_plot1d(std::make_pair(t_sim, y_sim_total), "with lines title 'REGIR'"s);
+		p.add_plot1d(std::make_pair(t_sim_nr, y_sim_total_nr), "with lines title 'NextReaction'"s);
 	});
 }
 #endif
