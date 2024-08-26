c7afadc4fbb316372b3c8e0fc6a4fe4b1a1b3f83
Florian G. Pflug <florian.pflug@oist.jp>
2024-08-26 18:47:55 +0900

    Modified "profile" to match the python vs. C++ comparisons

--- epidemics/programs/profile.cpp
+++ epidemics/programs/profile.cpp
@@ -3,6 +3,8 @@
 #include "graph.h"
 #include "NextReaction.h"
 
+#include <chrono>
+
 using namespace std::string_literals;
 
 int program_profile(int argc, const char * argv[])
@@ -12,39 +14,48 @@ int program_profile(int argc, const char * argv[])
     // Parameters, which are network size (N), average number of contacts (K), 
     // mean infection transmission time (Mi) and variance (Vi),
     // mean reset time (Mr) and variance (Vr), number of steps to run (M)
-    const int N = (argc >= 3) ? atoi(argv[2]) : 1000000;
-    const std::size_t M = (argc >= 4) ? atoi(argv[3]) : 1000000;
-    const int K = 5;
-    const double Mi = 3;
-    const double Vi = 30;
-    const double Mr = 50;
-    const double Vr = 20;
+    const int N = 20000;
+    const std::size_t M = 100;
+    const int K0 = 6;
+    const double P_REWIRE = 0.1;
+    const double Mi = 5;
+    const double Vi = 3;
+    const double Mr = 11;
+    const double Vr = 5;
     std::cerr << "Running with parameters:" << std::endl;
-    std::cerr << "      N: " << N << " (network size)" << std::endl;
-    std::cerr << "      K: " << N << " (average degree)" << std::endl;
-    std::cerr << "  Mi/Vi: " << Mi << " / " << Vi << " (Infection time mean / variance)" << std::endl;
-    std::cerr << "  Mr/Vr: " << Mr << " / " << Vr << " (Reset time mean / variance)" << std::endl;
-    std::cerr << "      M: " << M << " (number of steps)" << std::endl;
+    std::cerr << "        N: " << N << " (network size)" << std::endl;
+    std::cerr << "        K: " << K0 << " (average degree)" << std::endl;
+    std::cerr << " P_REWIRE: " << P_REWIRE << " (average degree)" << std::endl;
+    std::cerr << "    Mi/Vi: " << Mi << " / " << Vi << " (Infection time mean / variance)" << std::endl;
+    std::cerr << "    Mr/Vr: " << Mr << " / " << Vr << " (Reset time mean / variance)" << std::endl;
+    std::cerr << "        M: " << M << " (number of steps)" << std::endl;
 
     // Create contact network graph
     std::cerr << "Creating network" << std::endl;
-    auto nw = erdos_reyni(N, K, engine);
+    auto nw = watts_strogatz(N, K0, P_REWIRE, engine);
 
     // Create transmission and reset time distributions
     std::cerr << "Creating simulator" << std::endl;
-    const auto psi = transmission_time_lognormal(Mi, Vi);
+    const auto psi = transmission_time_gamma(Mi, Vi);
     const auto rho = transmission_time_lognormal(Mr, Vr);
 
-    // Create simulation and specifiy initial set of infections
-    auto sim = simulate_next_reaction(nw, psi, &rho);
-    sim.add_infections({{0, 0.0}});
-
     // Run simulation for M steps
+    double total_runtime = 0.0;
     for(std::size_t i=0; i < M; ++i) {
-        if ((i+1) % 100000 == 0)
-            std::cerr << "  Step " << (i+1) << " / " << M << std::endl;
-        sim.step(engine);
+        // Create simulation and specifiy initial set of infections
+        auto sim = simulate_next_reaction(nw, psi, &rho, false, true, true);
+        sim.add_infections({{0, 0.0}});
+
+        const auto start = std::chrono::steady_clock::now();
+        while (true) {
+            auto r = sim.step(engine);
+            if (!r)
+                break;
+        }
+        const auto end = std::chrono::steady_clock::now();
+        total_runtime += std::chrono::nanoseconds(end - start).count() / 1e9;
     }
-    
+
+    std::cerr << "Time per simultion run: " << (total_runtime / M) << " seconds " << std::endl;
     return 0;
 }
