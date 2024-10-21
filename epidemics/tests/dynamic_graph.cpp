ce5f60602e08a26b281f71a1ee974321b9913a1b
Florian G. Pflug <florian.pflug@oist.jp>
2024-10-21 14:42:03 +0900

    Completed dynamic Erdös-Reyni, added missing checks

--- epidemics/tests/dynamic_graph.cpp
+++ epidemics/tests/dynamic_graph.cpp
@@ -28,8 +28,8 @@ TEST_CASE("dynamic Erdös-Reyni", "[dynamic_graph]") {
 	const int N = 20;
 	const int E = N*(N-1)/2;
 	const double P = 0.1;
-	const double TAU = 1;
-	const double TMAX = 10000/TAU;
+	const double TAU = 2;
+	const double TMAX = 1000*TAU;
 	
 	/* Keep a list of pairs (t, f) for every edge which stores the times
 	 * at which the edge appears (t, 1) or disappears (t, 0)
@@ -98,14 +98,12 @@ TEST_CASE("dynamic Erdös-Reyni", "[dynamic_graph]") {
 		/* Compute present probability and times between events */
 		std::vector<double> times_present;
 		std::vector<double> times_absent;
-		double p_present = 0;
-
 		const auto end = p.end();
 		auto curr = p.begin();
-		auto prev = curr++;
+		auto prev = p.end();
 		for(; curr != end; prev = curr++) {
-			const double dt = curr->first - prev->first;
-			REQUIRE(prev->second != curr->second);
+			const double dt = curr->first - ((prev != p.end()) ? prev->first : 0.0);
+			REQUIRE(((prev == p.end()) || (prev->second != curr->second)));
 			if (curr->second) {
 				/* appeared */
 				times_absent.push_back(dt);
@@ -113,48 +111,45 @@ TEST_CASE("dynamic Erdös-Reyni", "[dynamic_graph]") {
 			else{
 				/* disappeared */
 				times_present.push_back(dt);
-				p_present += dt / TMAX;
 			}
 		}
-		if (prev->second) {
-			/* last event: appeared */
-			p_present += (TMAX - prev->first) / TMAX;
-		}
-		
-		const double time_present = std::accumulate(times_present.begin(), times_present.end(), 0.0) / times_present.size();
-		const double time_absent = std::accumulate(times_absent.begin(), times_absent.end(), 0.0) / times_absent.size();
 
-		/* Test that we see the exepcted number of events, mean present/absent times and presence probability,
-		 *   No of events: Counting process with mean waiting time mu = 1/alpha + 1/beta,
+		const double p_present = std::accumulate(times_present.begin(), times_present.end(), 0.0) / TMAX;
+		const double avg_time_present = std::accumulate(times_present.begin(), times_present.end(), 0.0) / times_present.size();
+		const double avg_time_absent = std::accumulate(times_absent.begin(), times_absent.end(), 0.0) / times_absent.size();
+
+		/* Test that we see the exepcted number of events (where an edge first appearing and then disappearing is
+		 * a single event), mean present/absent times and presence probability. The statistics are the following:
+		 *   Edge process: Markov process with rate alpha = P/TAU of edge appearing, beta = (1-P)/TAU disappearing,
+		 *   No of events: Counting process with mean waiting time mu = (1/alpha + 1/beta) / 2,
 		 *                 variance of waiting time sigma^2 = 1/alpha^2 + 1/beta^2. The mean and
 		 *                 variance of the no. of events is then TMAX/mu and TMAX*sigma^2 / mu^3
 		 *                 as TMAX goes to infinity (per counting process theory)
-		 *   Avg(present): Average of exponentially distributed waiting times with mean 1/(TAU*(1-P))
-		 *   Avg(absent) : Average of exponentially distributed waiting times with mean 1/(TAU*P)
-		 * We use a simple z-test, i.e. assume normality and known variance of all tested quantities!
+		 *   Avg(present): Average of exponentially distributed waiting times with mean TAU/(1-P)
+		 *   Avg(absent) : Average of exponentially distributed waiting times with mean TAU/P
+		 *   P(present)  : Scaled sum of exponentially distributed waiting times with mean 1/(TAU*(1-P))
+		 * We use a simple z-test, i.e. assume normality and known variance of all tested quantities.
+		 * Since we test each edge separately, the p-value threshold of 0.01 is Bonferroni-corrected by
+		 * dividing by the number of edges.
 		 */
-		/* TODO: This test fails for unknown reasons. Figure out why. */
-		//const double events = times_present.size() + times_absent.size();
-		//const double alpha = P/TAU, beta = 1/TAU - P/TAU;
-		//const double mu = 1/alpha + 1/beta;
-		//const double sigma2 = 1/pow(alpha, 2) + 1/pow(beta, 2);
-		//const double pval_events = ztest(events, sqrt(TMAX*sigma2/pow(mu, 3)), TMAX/mu);
-		//REQUIRE(pval_events >= 0.001);
 
-		const double pval_tpresent = ztest(time_present, 1 / (TAU*(1-P)*sqrt(times_present.size())), 1.0 / (TAU*(1-P)));
+		/* Number of events */
+		const double events = times_present.size();
+		const double alpha = P/TAU, beta = 1/TAU - P/TAU;
+		const double mu = 1/alpha + 1/beta;
+		const double sigma2 = 1/pow(alpha, 2) + 1/pow(beta, 2);
+		const double pval_events = ztest(events, sqrt(TMAX*sigma2/pow(mu, 3)), TMAX/mu);
+		REQUIRE(pval_events >= 0.01 / E);
+
+		/* Present and absent times */
+		const double pval_tpresent = ztest(avg_time_present, TAU/((1-P)*sqrt(times_absent.size())), TAU/(1-P));
 		REQUIRE(pval_tpresent >= 0.01 / E);
-		const double pval_tabsent = ztest(time_absent, 1 / (TAU*P*sqrt(times_absent.size())), 1.0 / (TAU*P));
+		const double pval_tabsent = ztest(avg_time_absent, TAU/(P*sqrt(times_absent.size())), TAU/P);
 		REQUIRE(pval_tabsent >= 0.01 / E);
-		/* TODO: Figure out the correct mean and variance of p_present */
-		//const double pval_ppresent = ztest(p_present, ?, P);
-		//REQUIRE(pval_ppresent > 0.01);
 
-		/*
-		std::cout << "events: " << events << " (pval " << std::setprecision(3) << pval_events << "), ";
-		std::cout << "Avg(present): " << std::setprecision(3) << time_present << " (pval " << std::setprecision(3) << pval_tpresent << "), ";
-		std::cout << "Avg(absent): " << std::setprecision(3) << time_absent << " (pval " << std::setprecision(3) << pval_tabsent << "), ";
-		std::cout << "P(present): " << std::setprecision(3) << p_present << " (pval " << std::setprecision(3) << pval_ppresent << ")" << std::endl;
-		*/
+		/* Presence probability */
+		const double pval_ppresent = ztest(p_present, std::sqrt((1-P)/TMAX), P);
+		REQUIRE(pval_ppresent > 0.01 / E);
 	}
 	
 	std::cout << "---" << std::endl;
