oist/NEXTNet

commit 61e9dfa3124c2434021840b4ce9e0ad2ddb94351

Fixed simulations on weighted networks. The previous approach was wrong, have to scale the edge multiplicity instead of time
Repurposed the field previously added to network_even_t called "infitesimal_duration" for general
edge weights, and renamed it to "weight". This now holds the duration in the case of infitesimal
contacts, and otherwise the weight of the edge. This fits well since both values effectively scale
the hazard rate.

nextnet/NextReaction.cpp
@@ -70,7 +70,7 @@ void simulate_next_reaction::notify_infected_node_neighbour_added(network_event_
 
 	/* Generate firing time of the newly added edge */
 	assert(event.time >= source_state->second.infection_time);
-	const double tau = psi.sample(engine, event.time - source_state->second.infection_time, 1);
+	const double tau = psi.sample(engine, event.time - source_state->second.infection_time, event.weight);
 	if (std::isnan(tau) || (tau < 0))
 		throw std::logic_error("transmission time must be positive");
 	const double t = event.time + tau;
@@ -107,7 +107,7 @@ void simulate_next_reaction::notify_infected_contact(network_event_t event, rng_
 
 	/* Computing tranmission probability */
 	assert(event.time >= source_state->second.infection_time);
-	const double p = psi.hazardrate(event.time - source_state->second.infection_time) * event.infitesimal_duration;
+	const double p = psi.hazardrate(event.time - source_state->second.infection_time) * event.weight;
 	if (!std::bernoulli_distribution(p)(engine))
 		return;
 
@@ -266,8 +266,14 @@ std::optional<epidemic_event_t> simulate_next_reaction::step_infection(const act
 		 * and processing it in order.
 		 */
 		assert(p.edges_concurrent || (weight == 1.0));
-		const int m = p.edges_concurrent ? 1 : neighbours_total;
-		const double tau = psi.sample(engine, 0, m) / weight;
+		double m;
+		if (p.edges_concurrent) {
+			m = weight;
+		} else {
+			assert(!nw_weighted);
+			m = neighbours_total;
+		}
+		const double tau = psi.sample(engine, 0, m);
         if (std::isnan(tau) || (tau < 0))
             throw std::logic_error("transmission times must be non-negative");
         const double t = next.time + tau;

nextnet/brownian_proximity_network.cpp
@@ -300,7 +300,8 @@ absolutetime_t brownian_proximity_network::next(rng_t& engine, absolutetime_t ma
 							/* Create event */
 							next_event = network_event_t {
 								.kind = network_event_kind::neighbour_removed,
-								.source_node = n.index, .target_node = n2.index, .time = current_time
+								.source_node = n.index, .target_node = n2.index, .weight = 1.0,
+								.time = current_time
 							};
 							/* And report it */
 							return next_event->time;
@@ -341,7 +342,8 @@ absolutetime_t brownian_proximity_network::next(rng_t& engine, absolutetime_t ma
 									/* Create event and return */
 									next_event = network_event_t {
 										.kind = network_event_kind::neighbour_added,
-										.source_node = n.index, .target_node = n2.index, .time = current_time
+										.source_node = n.index, .target_node = n2.index, .weight = 1.0,
+										.time = current_time
 									};
 									return next_event->time;
 								}

nextnet/temporal_network.cpp
@@ -89,7 +89,8 @@ empirical_temporal_network::empirical_temporal_network(std::string path_to_file,
 					.kind = network_event_kind::neighbour_added,
 					.source_node = src,
 					.target_node = dst,
-					.time = time - dt/2
+					.weight = 1.0,
+					.time = time - dt/2,
 				};
 				event_queue.push_back(ev1);
 
@@ -97,6 +98,7 @@ empirical_temporal_network::empirical_temporal_network(std::string path_to_file,
 					.kind = network_event_kind::neighbour_removed,
 					.source_node = src,
 					.target_node = dst,
+					.weight = 1.0,
 					.time = time + dt/2
 				};
 				event_queue.push_back(ev2);
@@ -107,8 +109,8 @@ empirical_temporal_network::empirical_temporal_network(std::string path_to_file,
 					.kind = network_event_kind::instantenous_contact,
 					.source_node = src,
 					.target_node = dst,
-					.time = time,
-					.infitesimal_duration = dt
+					.weight = dt,
+					.time = time
 				};
 				event_queue.push_back(ev);
 				break;
@@ -348,6 +350,7 @@ absolutetime_t temporal_sirx_network::next(rng_t& engine, absolutetime_t)
 				.kind = network_event_kind::neighbour_removed,
 				.source_node = n,
 				.target_node = nn,
+				.weight = 1.0,
 				.time = next_time
 			});
 		}
@@ -513,11 +516,13 @@ std::optional<network_event_t> temporal_erdos_reyni::step(rng_t& engine, absolut
 		remove_edge(src, neighbour_index);
 		reverse_edge_event = network_event_t {
 			.kind = network_event_kind::neighbour_removed,
-			.source_node = dst, .target_node = src, .time = next_time
+			.source_node = dst, .target_node = src, .weight = 1.0,
+			.time = next_time
 		};
 		return network_event_t {
 			.kind = network_event_kind::neighbour_removed,
-			.source_node = src, .target_node = dst, .time = next_time
+			.source_node = src, .target_node = dst, .weight = 1.0,
+			.time = next_time
 		};
 	}
 }
@@ -865,8 +870,9 @@ std::optional<network_event_t> activity_driven_network::step(rng_t& engine, abso
 	}
 
 
-	return network_event_t{
-					.kind = next.kind,
-					.source_node = next.source_node, .target_node = next.target_node, .time = next.time
-				};
+	return network_event_t {
+		.kind = next.kind,
+		.source_node = next.source_node, .target_node = next.target_node, .weight = 1.0,
+		.time = next.time
+	};
 }

nextnet/tests/temporal_nextreaction.cpp
@@ -56,6 +56,7 @@ namespace{
 				.kind = edge_present ? network_event_kind::neighbour_added : network_event_kind::neighbour_removed,
 				.source_node = 0,
 				.target_node = 1,
+				.weight = 1.0,
 				.time = t
 			};
 		}

nextnet/types.h
@@ -114,8 +114,8 @@ struct network_event_t {
 	network_event_kind kind = network_event_kind::none;
 	node_t source_node = -1;
 	node_t target_node = -1;
+	double weight = 1.0;
 	absolutetime_t time = INFINITY;
-	double infitesimal_duration = NAN;
 };
 
 /**

commit d0b5f1b4df69a02043c36a4f28e02a7a18c60f0e

Improved the fix of commit 61e9dfa3 from commit ffab79ec
Commit 61e9dfa3 broke the next reaction schema, commit ffab79ec fixed it but
possibly missed some cases that might have still been broken. This commit
attempts a better fix

nextnet/NextReaction.cpp
@@ -143,10 +143,10 @@ std::optional<epidemic_event_t> simulate_next_reaction::step_infection(const act
      * activate the next outgoing edge with the same originating node, called a *sibling*
      * edge.
      */
-    if (next.neighbours_remaining > 0 && !nw_weighted) {
+    if (next.neighbours_remaining > 0) {
 		/* Should never happen if we're making sibling edges active concurrently */
 		assert(!p.edges_concurrent);
-		assert(nw_weighted == nullptr);
+		assert(!nw_weighted);
 		
         /* This only occurs for infection edges, reset self-loops have no neighbours */
         const node_t neighbour_id = next.source_permutation[next.neighbour_index + 1];
@@ -263,17 +263,15 @@ std::optional<epidemic_event_t> simulate_next_reaction::step_infection(const act
 		 * If we're adding edges concurrently, however, we simply sample
 		 * from the original psi distribution. The minimum in this case is
 		 * the implicit result of putting all the times into the reaction queue,
-		 * and processing it in order.
+		 * and processing it in order. On weighted networks, only concurrent
+		 * edge mode is supported. There, the "weight" represents a (fractional)
+		 * multiplicity of edges, i.e. a weight of two means two parallel edges.
+		 * The time until an edge fires is (for an integral weight) therefore
+		 * also a minimal over several i.i.d draws of the unmodified distribution.
 		 */
 		assert(p.edges_concurrent || (weight == 1.0));
-		double m;
-		if (p.edges_concurrent) {
-			m = weight;
-		} else {
-			assert(!nw_weighted);
-			m = neighbours_total;
-		}
-		const double tau = psi.sample(engine, 0, m);
+		assert(p.edges_concurrent || !nw_weighted);
+		const double tau = psi.sample(engine, 0, p.edges_concurrent ? weight : neighbours_total);
         if (std::isnan(tau) || (tau < 0))
             throw std::logic_error("transmission times must be non-negative");
         const double t = next.time + tau;
@@ -290,7 +288,7 @@ std::optional<epidemic_event_t> simulate_next_reaction::step_infection(const act
             if (!p.edges_concurrent)
                 e.source_permutation = std::move(pi);
             e.neighbour_index = 0;
-			e.neighbours_remaining = m - 1;
+			e.neighbours_remaining = p.edges_concurrent ? 0 : (neighbours_total - 1);
 			push_edge(e);
         }
     }