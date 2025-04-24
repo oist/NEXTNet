ac9502804923ec9dfa3c50a1bfd5138644822bc0
Florian G. Pflug <fgp@phlo.org>
2025-04-24 23:03:03 +0900

    Use gzcat instead of zcat to decompress gz files

--- nextnet/factories/factories.cpp
+++ nextnet/factories/factories.cpp
@@ -215,7 +215,7 @@ istream_ref istream_ref::parse(const std::string& path) {
 
 	if (r.path.extension() == ".gz")
 		r.file = std::make_shared<redi::ipstream>(
-			"zcat", std::vector<std::string> { "zcat", path },
+			"gzcat", std::vector<std::string> { "gzcat", path },
 			redi::pstreambuf::pstdout);
 	else
 		r.file = std::make_shared<std::ifstream>(path);
