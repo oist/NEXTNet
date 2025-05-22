f3b6a5d4d54aa3b757e52ce1c371064d520efb38
Florian G. Pflug <florian.pflug@oist.jp>
2025-05-22 13:41:46 +0900

    Added time distribution infectiousness, polynomial_rate to CLI simulator

--- nextnet/factories/time.cpp
+++ nextnet/factories/time.cpp
@@ -19,6 +19,9 @@ DECLARE_ARGUMENT_3(mean, double, std::nullopt);
 DECLARE_ARGUMENT_3(variance, double, std::nullopt);
 DECLARE_ARGUMENT_3(shape, double, std::nullopt);
 DECLARE_ARGUMENT_3(scale, double, std::nullopt);
+DECLARE_ARGUMENT_5(taus, std::vector<double>, std::nullopt, parse_vector<double>, render_vector<double>);
+DECLARE_ARGUMENT_5(lambdas, std::vector<double>, std::nullopt, parse_vector<double>, render_vector<double>);
+DECLARE_ARGUMENT_5(coeffs, std::vector<double>, std::nullopt, parse_vector<double>, render_vector<double>);
 DECLARE_ARGUMENT_3(tau, double, std::nullopt);
 
 /* Time distribution factory */
@@ -28,6 +31,8 @@ factory<transmission_time> time_factory = factory<transmission_time>("time")
 	.add<transmission_time_gamma, mean, variance, pinf>("gamma")
 	.add<transmission_time_exponential, lambda>("exponential")
 	.add<transmission_time_weibull, shape, scale, pinf>("weibull")
+	.add<transmission_time_infectiousness, taus, lambdas>("infectiousness")
+	.add<transmission_time_polynomial_rate, coeffs>("polynomial_rate")
 	.add<transmission_time_deterministic, tau>("deterministic");
 
 } /* namespace factories */
